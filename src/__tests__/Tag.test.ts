b4f5e343c8c07afd09e2790c616ba550e0243269
Vitaliy Khalrov <v.kharlov@tbank.ru>
2025-12-22 17:23:22 +0300

    Написал новый класс Tag и тесты к нему

---
+++ src/__tests__/Tag.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from 'vitest';
+import Tag from '../components/Tag/Tag';
+
+test('br', () => {
+  expect(new Tag("br").toString()).toBe('<br />')
+})
+
+test('img', () => {
+  expect(new Tag("img", { src: "path/to/image" }).toString()).toBe('<img src="path/to/image" />')
+})
+test('input', () => {
+  expect(new Tag("input", { type: "submit", value: "Save" }).toString()).toBe('<input type="submit" value="Save" />')
+})
+test('label', () => {
+  expect(new Tag("label", {}, "Email").toString()).toBe('<label>Email</label>')
+})
+test('label 2', () => {
+  expect(new Tag("label", { for: "email" }, "Email").toString()).toBe('<label for="email">Email</label>')
+})
+test('div', () => {
+  expect(new Tag("div").toString()).toBe('<div></div>')
+})
---
+++ src/components/Tag/Tag.ts
@@ -0,0 +1,44 @@
+type TParams = Record<string, string>;
+
+export default class Tag {
+    private singleTags: string[] =[
+        'area',
+        'base',
+        'br',
+        'col',
+        'embed',
+        'hr',
+        'img',
+        'input',
+        'link',
+        'meta',
+        'source',
+        'track',
+        'wbr',
+    ];
+    private name: string;
+    private params: TParams;
+    private content: string;
+    private attributes: string;
+    private isSingleTag: boolean;
+    constructor(name: string, params?: TParams, content?: string) {
+        this.name = name;
+        this.params = params ?? {};
+        this.content = content || '';
+        this.isSingleTag = this.singleTags.includes(name);
+        if (Object.keys(this.params).length > 0) {
+            this.attributes = ' ' + Object.entries(this.params)
+                .map(([key, value]) => `${key}="${value}"`)
+               .join(' ');
+        } else {
+            this.attributes = '';
+        }
+
+    }
+
+    public toString () {
+        return this.isSingleTag
+           ? `<${this.name}${this.attributes} />`
+           : `<${this.name}${this.attributes}>${this.content}</${this.name}>`;
+    }
+}
